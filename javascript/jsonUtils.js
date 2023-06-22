42ebc19dfb229eb66fd946c8c7f4149cef5838a1
Beniamin Hozan <benjaminhozan@gmail.com>
2023-06-22 23:01:49 +0300

    Update jsonUtils.js

--- javascript/jsonUtils.js
+++ javascript/jsonUtils.js
@@ -5,7 +5,7 @@ export function addPathPrefixToImgSrc(jsonData, prefix) {
     });
 }
 
-export funtion addPathPrefix(jsonData, prefix){
+export function addPathPrefix(jsonData, prefix){
     return jsonData.map(item => {
         item.image = "../" + prefix + item.image.substring(3);
         item.link = "../" + prefix + item.link.substring(3);
